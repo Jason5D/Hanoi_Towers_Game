86ee2ccd56f9cfe1c2a6d19e095f3b3439be2e7b
Jason <jason5donoghue@gmail.com>
2023-10-10 08:33:47 +0100

    game logic down with some bugs

--- script.py
+++ script.py
@@ -42,10 +42,31 @@ def get_input():
                 if user_input == choices[i]:
                     return stacks[i]
 
+
 # Play the Game
 num_user_moves = 0
 
 while right_stack.get_size() != num_disks:
     print("\n\n\n...Current Stacks...")
     for stack in stacks:
-        stacks.print_items(stack)
+        stack.print_items()
+    while True:
+        print("\nWhich stack do you want to move from?\n")
+        from_stack = get_input()
+        print("\nWhich stack do you want to move to?\n")
+        to_stack = get_input()
+        if from_stack.is_empty():
+            print("\n\nInvalid Move. Try Again")
+        elif to_stack.is_empty() or from_stack.peak() < to_stack.peak():
+            disk = from_stack.pop()
+            to_stack.push(disk)
+            num_user_moves += 1
+            break
+        else:
+            print("\n\nInvalid Move. Try Again")
+
+print(
+    "\n\nYou completed the game in {0} moves, and the optimal number of moves is {1}".format(
+        num_user_moves, num_optimal_moves
+    )
+)
